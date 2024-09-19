1b2dda84d0716746b9875c97135b457701b0b84b
Mustafa Azad <mustafa.azad@antrepriz.com>
2024-09-19 08:49:09 +0530

    init: initialized app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <Text className="bg-orange-300 flex-1">
+      Hello World!!
+    </Text>
+  );
+}
+
+export default App;
